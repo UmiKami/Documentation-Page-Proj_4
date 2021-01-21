4bbae2239c4317f11b3d4450c3341e108b6ce68f
UmiKami <ggoku02@gmail.com>
2021-01-21 13:51:41 -0500

    Intro Section Styling Finished, Code Highlight lib added

--- app.js
+++ app.js
@@ -4,10 +4,24 @@ const daySphere = document.querySelector(".day-sphere");
 const nightSphere = document.querySelector(".night-sphere")
 const root = document.documentElement;
 
+function changeCSS(cssFile, cssLinkIndex) {
+
+    let oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
+
+    let newlink = document.createElement("link");
+    newlink.setAttribute("rel", "stylesheet");
+    newlink.setAttribute("type", "text/css");
+    newlink.setAttribute("href", cssFile);
+
+    document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+}
+
 themeToggleBtn.addEventListener("click", function(e){
     if(daySphere.classList.contains("slide-right")){
         daySphere.classList.remove("slide-right");
         nightSphere.classList.remove("show-night");
+
+        changeCSS(`https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.5.0/styles/a11y-light.min.css`, 1)
         
         root.style.setProperty(`--primary-color`, "var(--primary-color-light)");
         root.style.setProperty(`--secondary-color`, "var(--secondary-color-light)");
@@ -16,10 +30,13 @@ themeToggleBtn.addEventListener("click", function(e){
         root.style.setProperty(`--head-color`, "var(--head-color-light)");
         root.style.setProperty(`--toggle-color`, "var(--toggle-color-light)");
         root.style.setProperty(`--head-header-color`, "var(--head-header-color-light)");
-        
+        root.style.setProperty(`--code-bg-color`, "var(--code-bg-color-light)");        
+        root.style.setProperty(`--btn-ex-color`, "var(--btn-ex-color-light)");        
     }else{
         daySphere.classList.add("slide-right");
         nightSphere.classList.add("show-night");
+        
+        changeCSS(`https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.5.0/styles/a11y-dark.min.css`, 1)
         
         root.style.setProperty(`--primary-color`, "var(--primary-color-dark)");
         root.style.setProperty(`--secondary-color`, "var(--secondary-color-dark)");
@@ -28,6 +45,8 @@ themeToggleBtn.addEventListener("click", function(e){
         root.style.setProperty(`--head-color`, "var(--head-color-dark)");
         root.style.setProperty(`--toggle-color`, "var(--toggle-color-dark)");
         root.style.setProperty(`--head-header-color`, "var(--head-header-color-dark)");
+        root.style.setProperty(`--code-bg-color`, "var(--code-bg-color-dark)");
+        root.style.setProperty(`--btn-ex-color`, "var(--btn-ex-color-dark)");        
         
     }
 })
@@ -39,4 +58,14 @@ const nav = document.querySelector("#navbar");
 
 navBtn.addEventListener("click", function(){
     nav.classList.toggle("show");
+})
+
+////////////////////////////////////////////////////////////////////////////////////// MAIN SECTION
+
+// intro
+
+const introExBtn = document.querySelector(".intro-ex-btn");
+
+introExBtn.addEventListener("click", function(){
+    alert("Hello World!")
 })
